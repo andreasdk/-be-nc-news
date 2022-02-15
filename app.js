12f2e719aafe560dde8d9c0eeb40ea26dd0902ce
Andrew Kennedy <andrew.kennedy27@outlook.com>
2022-02-15 14:36:11 +0000

    added custom error handling

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const {getArticleByID} = require('./controllers/articleController');
 const {getTopics} = require('./controllers/topicsController');
 const {
+  handlePSQLErrors,
   handle500Errors,
+  handleCustomErrors
 } = require("./errors/errors");
 
 const app = express();
@@ -15,6 +17,9 @@ app.all("/*", (req, res, next) => {
   res.status(404).send({ msg: "Page Not Found" });
 });
 
+app.use(handlePSQLErrors);
+app.use(handleCustomErrors);
 app.use(handle500Errors);
+
 
 module.exports = app;
--- errors/errors.js
+++ errors/errors.js
@@ -1,3 +1,17 @@
+exports.handlePSQLErrors = (err, req, res, next) => {
+    if (err.code === "22P02")
+      res.status(400).send({ msg: "Bad Request" });
+    else if (err.code === "23503")
+      res.status(404).send({ msg: "Page Not Found" });
+    else next(err);
+};
+
+exports.handleCustomErrors = (err, req, res, next) => {
+    if (err.status)
+      res.status(err.status).send({ msg: err.msg || "Page Not Found" });
+    else next(err);
+};
+
 exports.handle500Errors = (err, req, res, next) => {
     res.status(500).send({ msg: "Internal server error" });
 };
